72877905017ce825ca682260444ece6bab017402
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-10-25 09:01:45 -0300

    10° Req._Recover favorite songs & update the list

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,27 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Loading from '../pages/Loading';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default class Card extends React.Component {
   constructor() {
     super();
 
     this.onAddSong = this.onAddSong.bind(this);
+    this.onGetFavoriteSongs = this.onGetFavoriteSongs.bind(this);
+    this.isFavoriteMusic = this.isFavoriteMusic.bind(this);
 
-    this.state = { loading: false, isChecked: false };
+    this.state = {
+      loading: false,
+      isChecked: false,
+      favoriteArtistList: [],
+    };
+  }
+
+  componentDidMount() {
+    this.onGetFavoriteSongs();
+  }
+
+  onGetFavoriteSongs() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const response = await getFavoriteSongs();
+        await response;
+        this.setState({
+          favoriteArtistList: response,
+          loading: false });
+        this.isFavoriteMusic();
+      },
+    );
   }
 
   onAddSong() {
     const { trackId } = this.props;
+    const { isChecked } = this.state;
 
     this.setState(
       { loading: true },
       async () => {
         await addSong(trackId);
-        this.setState({ loading: false, isChecked: true });
+        this.setState(isChecked === true
+          ? { isChecked: false }
+          : { isChecked: true });
+        this.setState({ loading: false });
       },
     );
+  }
+
+  isFavoriteMusic() {
+    const { trackId } = this.props;
+    const { favoriteArtistList } = this.state;
+
+    favoriteArtistList
+      .forEach((id) => (id === trackId && this.setState({ isChecked: true })));
   }
 
   render() {
